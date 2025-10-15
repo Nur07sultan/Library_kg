10b0ef36fca3acce76b06803dda318de990d443b
Nur07sultan <Aibeknurel07@gmail.com>
2025-10-15 19:43:05 +0600

    сделал дз

--- clothes/urls.py
+++ clothes/urls.py
@@ -5,8 +5,13 @@ app_name = 'clothes'
 
 urlpatterns = [
     path('', views.all_clothes, name='all_clothes'),
+    path('men/', views.men_clothes, name='men_clothes'),
+    path('women/', views.women_clothes, name='women_clothes'),
+    path('kids/', views.kids_clothes, name='kids_clothes'),
+    path('search/', views.search_clothes, name='search_clothes'),
     path('ajax/', views.clothes_ajax, name='clothes_ajax'),
 ]
+
 
 
 
